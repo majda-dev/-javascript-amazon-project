0976746171316bba11b2ae62d4d43372332bc0c2
Majda Alaabouch <112486265+majda-dev@users.noreply.github.com>
2024-02-15 14:56:54 +0000

    Make delivdelivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,9 @@
-import { cart, removeFromCart, calculateCartQuantity, updateQuantity} from '../data/cart.js';
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions} from '../data/deliveryOptions.js';
+
 
 const today = dayjs();
 let cartSummaryHTML = '';
@@ -29,8 +30,8 @@ cart.forEach((cartItem) => {
   });
 
   const today = dayjs(); 
-    const deliverydate = today.add(deliveryOption.deliveryDays, 'days');
-    const dateString = deliverydate.format('dddd, MMMM D');
+  const deliverydate = today.add(deliveryOption.deliveryDays, 'days');
+  const dateString = deliverydate.format('dddd, MMMM D');
 
   cartSummaryHTML += `
     <div class="cart-item-container
@@ -92,7 +93,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
 
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
     html +=
-      `<div class="delivery-option">
+      `<div class="delivery-option js-delivery-option"
+      data-product-id= "${matchingProduct.id}"
+      data-delivery-option-id= "${deliveryOption.id}">
         <input type="radio" ${isChecked ? 'checked' : ''}
           class="delivery-option-input"
           name="delivery-option-${matchingProduct.id}">
@@ -170,5 +173,13 @@ document.querySelectorAll('.js-save-link')
       quantityLabel.innerHTML = newQuantity;
 
       updateCartQuantity();
+    });
+  });
+
+  document.querySelectorAll('.js-delivery-option')
+  .forEach((element) => {
+    element.addEventListener('click', () => {
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
     });
   });
